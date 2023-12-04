8c62306320dd42ba7e32587fd6a44412c3d04e4a
mayuri <mayuri.jadhav@dsm.com>
2023-12-04 11:37:07 +0100

    initial schema file updates

--- src/main/resources/db/changelog/changes/001-initial-schema.sql
+++ src/main/resources/db/changelog/changes/001-initial-schema.sql
@@ -5,5 +5,12 @@ CREATE TABLE Users
     address     VARCHAR,
     email       VARCHAR,
     phonenumber Varchar
-)
+);
 
+CREATE TABLE Organisation
+(
+    id          Long PRIMARY KEY,
+    name        VARCHAR,
+);
+
+INSERT INTO Organisation(id,name) VALUES (1,'Accenture');
