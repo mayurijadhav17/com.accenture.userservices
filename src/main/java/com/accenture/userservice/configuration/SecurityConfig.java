6a4d73edb918365a444b4721013d4b2b9aa2c35f
mayuri <mayuri.jadhav@dsm.com>
2023-12-14 10:04:45 +0100

    basic authentication

---
+++ src/main/java/com/accenture/userservice/configuration/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.accenture.userservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+ 
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails admin = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("password")
+            .roles("ADMIN")
+            .build();
+    UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+    return new InMemoryUserDetailsManager(admin,user);
+  }
+  
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeRequests(authorizeRequests -> authorizeRequests.requestMatchers("/api/user/**",
+                    "/api/organisation/**","/h2/console/**","/api/external.api/**")
+            .permitAll()).csrf((csrf) -> csrf.disable());
+    httpSecurity.headers(headers -> headers.frameOptions().disable());
+    return httpSecurity.build();
+  }
+}
