b7dc59f3f0337dfa2d85b0969d6e4fc598669597
mayuri <mayuri.jadhav@dsm.com>
2024-01-03 23:13:09 +0100

    code refactor

--- src/main/java/com/accenture/userservice/EmailSendApplication.java
+++ src/main/java/com/accenture/userservice/EmailSendApplication.java
@@ -4,6 +4,7 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import lombok.extern.slf4j.Slf4j;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 @Slf4j
@@ -15,8 +16,7 @@ public class EmailSendApplication {
     wireMockServer.start();
     
     wireMockServer.stubFor(WireMock.post(WireMock.urlEqualTo("/email/send"))
-            .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
-                    .withStatus(200).withBody("{\"token\":\"123456\"}/n")));
+            .willReturn(aResponse().withStatus(200)));
     log.info("Server Start");
     log.error(wireMockServer.toString());
     log.error(wireMockServer.getStubMappings().toString());
