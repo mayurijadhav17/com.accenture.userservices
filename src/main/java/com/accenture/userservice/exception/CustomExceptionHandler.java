4aa64be1a8ffdeac3010ddbd1bf751cb19cc321d
mayuri <mayuri.jadhav@dsm.com>
2024-01-18 13:41:43 +0100

    code refactor

--- src/main/java/com/accenture/userservice/exception/CustomExceptionHandler.java
+++ src/main/java/com/accenture/userservice/exception/CustomExceptionHandler.java
@@ -1,44 +1,34 @@
 package com.accenture.userservice.exception;
 
 import com.accenture.userservice.dto.ErrorDto;
+import com.accenture.userservice.model.ErrorCodeEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.nio.file.AccessDeniedException;
-
 @Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
 
-public CustomExceptionHandler()
-{
 
-}
     @ExceptionHandler(ServiceRuntimeException.class)
     public @ResponseBody ErrorDto handleServiceRuntimeException(final ServiceRuntimeException exception) {
         var errorDto = new ErrorDto();
         errorDto.setErrorMessage(exception.getMessage());
         errorDto.setErrorCode(exception.getErrorCode());
-        log.error(exception.getStackTrace().toString());
+        log.error("Service Exception", exception);
         return errorDto;
     }
-//add constructor for common code errordto
+
+
     @ExceptionHandler(Throwable.class)
     public @ResponseBody ErrorDto handleNullPointerException(final Throwable exception) {
         var errorDto = new ErrorDto();
         errorDto.setErrorMessage(exception.getMessage());
-       // errorDto.setErrorCode(ErrorCodeEnum.GENERAL_ERROR);
-       log.error("Internal Server Error",exception);
+        errorDto.setErrorCode(ErrorCodeEnum.GENERAL_ERROR.name());
+        log.error("Internal Server Error", exception);
         return errorDto;
     }
-    @ExceptionHandler(AccessDeniedException.class)
-    public @ResponseBody ErrorDto handleAuthorizationException(final AccessDeniedException exception) {
-        var errorDto = new ErrorDto();
-        errorDto.setErrorMessage(exception.getMessage());
-        //add custom error code
-        log.error(exception.getStackTrace().toString());
-        return errorDto;
-    }
+
 }
