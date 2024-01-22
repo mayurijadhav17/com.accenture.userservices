cee46cbee5367c4cee1c8ae1d61ab3fb4ed1f594
mayuri <mayuri.jadhav@dsm.com>
2024-01-22 15:50:25 +0100

    code refactor

--- src/main/java/com/accenture/userservice/model/ErrorCodeEnum.java
+++ src/main/java/com/accenture/userservice/model/ErrorCodeEnum.java
@@ -1,18 +1,16 @@
 package com.accenture.userservice.model;
 
 public enum ErrorCodeEnum {
-    EMAIL_EXISTS("Email address already exists"),
-    EMAIL_INVALID("Email address is invalid"),
-    USER_NOT_FOUND("User not found,id={0}"),
-    ORGANISATION_NOT_FOUND("Organisation not found "),
-
-
-    ORGANISATION_DOMAIN_EXISTS("Organisation domain is already exists"),
+    EMAIL_EXISTS("Email address already exists :{0}"),
+    EMAIL_INVALID("Email address is invalid :{0}"),
+    USER_NOT_FOUND("User not found :{0}"),
+    ORGANISATION_NOT_FOUND("Organisation not found :{0} "),
+    ORGANISATION_DOMAIN_EXISTS("Organisation domain is already exists :{0}"),
     SUCCESS("SUCCESS"),
     TOTAL_ATTEMPTS_OVER("Total attempts over for email verification"),
     TOKEN_MISMATCH("TOKEN_MISMATCH"),
     CODE_EXPIRED("CODE_EXPIRED"),
-    GENERAL_ERROR("Internal Server Error");
+    SERVER_ERROR("Internal Server Error");
 
 
     private String template;
